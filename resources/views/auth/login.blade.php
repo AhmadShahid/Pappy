577ccf17cff21a33b24797b0c2899cbfffce03b9
unknown <shahidahmad527@gmail.com>
2016-07-01 16:26:23 +0500

    change login screen design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <article class="sheet--sign-in recordable--balloon" data-behavior="sign_in_notice_container">
+        
+            <div class="flash-notice" data-behavior="flash_notice">
+                <div class="flash-notice__content">Sorry, we don’t recognize that email or username.</div>
             </div>
-        </div>
+
+            <header class="push--bottom centered">
+                <h3 class="flush--top push_half--bottom">Happy <time data-localized="true">Thursday</time>!</h3>
+
+                <p class="flush">
+                    Just enter your email address or username and we’ll get you right into Basecamp.
+                </p>
+                
+            </header>
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+
+                {!! csrf_field() !!}
+
+                <section>
+
+                    <div class="session-fields__attribute">
+                        <label class="session-fields__label" for="login_email">Email address or username</label>
+                        <input type="email" class="form-control" class="input--text u-focusable-input session-fields__email"  name="email" value="{{ old('email') }}" id="login_email" placeholder="julie@widgetco.com" required="required" autofocus="autofocus" autocapitalize="off" autocorrect="off" >
+                    </div>
+
+                    <div class="session-fields__attribute">
+                        <label class="session-fields__label" for="login_password">Password</label>
+                        <input type="password" class="form-control" class="input--text u-focusable-input session-fields__email"  name="password" value="{{ old('password') }}" id="login_password" required="required" autofocus="autofocus" autocapitalize="off" autocorrect="off" >
+                    </div>
+
+                    <div class="session-fields__attribute">
+                        <div class="checkbox">
+                            <label class="session-fields__label">
+                                <input type="checkbox" name="remember"> Remember Me
+                            </label>
+                        </div>
+                    </div>
+
+                    <input type="submit" name="commit" value="Log in" id="email-sign-in" class="session-fields__button action_button primary">
+
+                </section>
+
+            </form>
+
+        </article>
+
     </div>
-</div>
 @endsection

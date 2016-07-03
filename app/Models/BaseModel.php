387aa97e8bd8d47d602fb4a024553a4350c372b3
shahid ahmad <shahidahmad527@gmail.com>
2016-07-03 18:44:11 +0500

    add base model and add method to project mode

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes; 
+
+abstract class BaseModel extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends BaseModel
+{
+    
+	/**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'projects';
+
+    /**
+     * Show List of all the client 
+     * @param array
+     * @return array of clients
+    */
+
+    public function showAllProjects( $params ){
+
+        $results = $this; 
+
+        if( isset( $params['Search'] ) && !empty( $params['Search'] ) ) {
+            
+        }
+
+        $toTake = config('setting.pagination');  
+
+        return $results->sortBy()->paginate( $toTake );
+    }
+}
